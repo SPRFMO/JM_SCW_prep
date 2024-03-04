f2b80526eeb3db375edfa1059dc14a3614c4d59f
Jim Ianelli <jim.ianelli@gmail.com>
2024-03-04 07:27:41 -0800

    updated readme

--- mse/Demo_JM2OM.R
+++ mse/Demo_JM2OM.R
@@ -4,7 +4,7 @@
 # ===========================================================================================================
 
 # Tom Carruthers
-# Feb 2024 
+# Feb 2024
 
 # --- installation -------------------------------------------------------------
 
@@ -13,40 +13,50 @@
 
 # --- Prerequisites ------------------------------------------------------------
 
-setwd("C:/GitHub/jjm/assessment")
+setwd("~/_mymods/SPRFMO/jjm/assessment")
 
 library(tidyverse)
 library(flextable)
+install.packages("libr")
 library(libr)
+install.packages("fmtr")
 library(fmtr)
 library(abind)
 library(openMSE)
 
-source("..//mse/Source_JM2OM.r")   # load JM2OM source code
+source("../mse/Source_JM2OM.r")   # load JM2OM source code
 
 
 # --- Conditioning Model inputs / outputs --------------------------------------
 
 mod <- jjmR::readJJM("h1_1.00", path = "config", input = "input") # data, pars, MLE output
-mceval_file = "C:/temp/JM_mcmc/mceval.csv"                        # location of MCMC output of F and N at age
+mceval_file = "~/Downloads/mceval.csv"                        # location of MCMC output of F and N at age
+getwd()
+
+df1<-readr::read_csv(mceval_file)
+head(df1)
+glimpse(df2)
+df2 <- sample_frac(df1, size = .1)
+df2 <- df1 |> filter(mcdraw<33)
+write_csv(df2,"mceval.csv")
 
 
 # --- Make an OM using JM2OM ---------------------------------------------------
 
-OM = JM2OM(mod, mceval_file, 
+OM = JM2OM(mod, mceval_file,
            proyears = 50,          # 50 projection years
            interval = 2,           # update management every 2 years
            nsim = 32,              # only 32 sims for demo purposes
            datastart = 2012,       # only the last 10 years of data are submitted to derive statistical properties for index simulation
            check = T)              # visual check that reconstructed openMSE N and F matches conditioning model estimates
                                    #   NOTE: there is an apparent 1-year lag in the numbers at age in the
-                                   #   'check' figure, as openMSE numbers are reported as start of year. 
+                                   #   'check' figure, as openMSE numbers are reported as start of year.
 
 
 # --- plot the OM --------------------------------------------------------------
 
 plot(OM)                           # This is mostly just for seeing time series but documents all aspects of the model
-                                   #   NOTE: observation error characteristics for indices used in the assessment are 
+                                   #   NOTE: observation error characteristics for indices used in the assessment are
                                    #   calculated from the simulated observed data (see below)
 
 
@@ -74,7 +84,7 @@ DFO_proj(proj_simple)              # DFO projection 'Kobe' plot
 # --- Design a simple index ratio MP -------------------------------------------
 
 PPD_75 = proj_simple@PPD[[5]]                  # 5th MP was FMSYref 75
-endy = 91:100                                  # last 10 years of projection 
+endy = 91:100                                  # last 10 years of projection
 muIend = apply(PPD_75@AddInd[,,endy],1:2,mean) # average value of each simulated index over last 10 years
 Catend = apply(PPD_75@Cat[,endy],1,mean)
 rate = Catend / muIend
@@ -87,14 +97,14 @@ myIRMP = function(x, Data, reps=1, smthyrs = 3,                                #
                   TRP =     c(NA,  781,   0.249,  NA,  177,    7.99,   1194),  # 'target' index level
                   mult = 0.8,                                                  # multiplier of initial delta
                   useInd = c(NA, NA, 1, NA, NA, NA, 1)){                       # only #3 and #7 seems to have low CV....
-  
+
   thisyr = length(Data@Year)                                                   # index of most recent year in data
   subI = Data@AddInd[x,,thisyr-((smthyrs-1):0),drop=F]                         # take average of last smthyrs years
   curI = apply(subI, 2, mean)                                                  # simple average (could be a smoother etc)
   mod = curI / TRP; mod[mod>1]=1                                               # linear decline in catch below 'TRP' index level
   Rec=new('Rec')                                                               # make an object of class Recommendation
   Rec@TAC = mean(curI * deltas * useInd * mod, na.rm=T) * mult                 # populate the TAC slot with the mean catch rec across indices
-  Rec 
+  Rec
 }
 
 class(myIRMP) = "MP"
@@ -105,7 +115,7 @@ Pplot(proj_IR)
 
 # --- Assessments as MPs -------------------------------------------------------
 
-avail('MP')                             # some premade data rich assessment MPs in here 
+avail('MP')                             # some premade data rich assessment MPs in here
 avail('Assess')                         # possible assessment methods
 avail('HCR')                            # possible HCRs
 SSSP_4010 = make_MP("SP_SS","HCR40_10") # state-space surplus production with 40:10 rule
