f56f2fd5b52113be0963e56d21c8afc118608236
lee-qi <leeqi@uw.edu>
2024-10-30 18:05:26 -0700

    updated projection code for risk table

--- assessment/R/run_projections_tac.R
+++ assessment/R/run_projections_tac.R
@@ -2,8 +2,10 @@
 # h1 and h2 should be in different directories as results files will be over-written
 
 tac <- 1242
-tac15 <- tac*1.15
-tac20 <- tac*1.2
+tac15 <- tac * 1.15
+tac20 <- tac * 1.2
+tac75 <- tac * 1.75
+tac100 <- tac * 2
 
 h <- "h2"
 finmodnm <- "1.07"
@@ -22,6 +24,14 @@ if(h=="h2") file.rename(from="For_R_2.rep", to=paste0(h,"_For_R_2_tac15.rep"))
 system(paste0("./jjm -ind ", h, "_", finmodnm, ".ls.ctl -binp jjm.bar -phase 22 -tac ", tac20, " -sdonly"))
 file.rename(from="For_R_1.rep", to=paste0(h,"_For_R_1_tac20.rep"))
 if(h=="h2") file.rename(from="For_R_2.rep", to=paste0(h,"_For_R_2_tac20.rep"))
+
+system(paste0("./jjm -ind ", h, "_", finmodnm, ".ls.ctl -binp jjm.bar -phase 22 -tac ", tac75, " -sdonly"))
+file.rename(from="For_R_1.rep", to=paste0(h,"_For_R_1_tac75.rep"))
+if(h=="h2") file.rename(from="For_R_2.rep", to=paste0(h,"_For_R_2_tac75.rep"))
+
+system(paste0("./jjm -ind ", h, "_", finmodnm, ".ls.ctl -binp jjm.bar -phase 22 -tac ", tac100, " -sdonly"))
+file.rename(from="For_R_1.rep", to=paste0(h,"_For_R_1_tac100.rep"))
+if(h=="h2") file.rename(from="For_R_2.rep", to=paste0(h,"_For_R_2_tac100.rep"))
 
 
 # Copypasta tac.rep files to jjm/assessment/risk_tables
@@ -43,52 +53,87 @@ h2_mod <- readJJM(paste0("h2_",finmodnm), path = "config", input = "input") %>% 
 mods_com_ls <- combineModels(h1_modls,h2_modls)
 mods_com <- combineModels(h1_mod,h2_mod)
 
-quants_hist <- mods_com_ls %>% .reshapeJJM2(what="catchProj") %>%
-				rename(catch=data) %>%
-				left_join(mods_com_ls %>% .reshapeJJM2(what="ssbProj") %>% rename(ssb=data)) %>%
-				mutate(stocks=str_remove(stocks,"Stock_"),
-						model=str_remove(model,paste0("_",finmodnm,".ls"))) %>%
-				rename(stock=stocks, hyp=model)
+quants_hist <- mods_com_ls %>%
+  .reshapeJJM2(what="catchProj") %>%
+  rename(catch=data) %>%
+  left_join(
+    mods_com_ls %>%
+    .reshapeJJM2(what="ssbProj") %>%
+    rename(ssb=data)
+  ) %>%
+  mutate(stocks=str_remove(stocks,"Stock_"),
+    model=str_remove(model,paste0("_",finmodnm,".ls"))) %>%
+  rename(stock=stocks, hyp=model)
 
 quants_msy <- mods_com %>%
-				get_msy_mt() %>%
-				group_by(model, stock) %>%
-				summarise(bmsy=unique(bmsy)) %>%
-				ungroup() %>%
-				mutate(hyp=str_remove(model,paste0("_",finmodnm)),
-						stock=str_remove(stock,"Stock_")) %>%
-				select(-model) %>%
-				as_tibble()
+  get_msy_mt() %>%
+  group_by(model, stock) %>%
+  summarise(bmsy=unique(bmsy)) %>%
+  ungroup() %>%
+  mutate(hyp=str_remove(model,paste0("_",finmodnm)),
+    stock=str_remove(stock,"Stock_")) %>%
+  select(-model) %>%
+  as_tibble()
 
 repfilenames <- list.files(here("risk_tables"))[grep(".rep",list.files(here("risk_tables")))]
 
 fn_pullfuts <- function(nm) {
-	tmp <- read_rep(here("risk_tables",nm))
+  tmp <- read_rep(here("risk_tables",nm))
 
-	ind_cat <- grep("Catch_fut_5",names(tmp))
-	ind_ssb <- grep("SSB_fut_5",names(tmp))
+  ind_cat <- grep("Catch_fut_5",names(tmp))
+  ind_ssb <- grep("SSB_fut_5",names(tmp))
 
-	cat_fut <- tmp[[ind_cat]] %>%
-				as_tibble() %>%
-				rename(year=V1,catch=V2)
+  cat_fut <- tmp[[ind_cat]] %>%
+    as_tibble() %>%
+    rename(year=V1,catch=V2)
 
-	ssb_fut <- tmp[[ind_ssb]] %>%
-				as_tibble() %>%
-				rename(year=V1, ssb=V2, sd=V3, lb=V4, ub=V5)
+  ssb_fut <- tmp[[ind_ssb]] %>%
+    as_tibble() %>%
+    rename(year=V1, ssb=V2, sd=V3, lb=V4, ub=V5)
 
-	quant_fut <- full_join(cat_fut,ssb_fut) %>%
-					mutate(file=nm)
+  quant_fut <- full_join(cat_fut,ssb_fut) %>%
+    mutate(file=nm)
 
-	return(quant_fut)
+  return(quant_fut)
 }
 
+fn_pullfutF <- function(nm, scen) {
+  tmp <- read_rep(here("risk_tables",nm))
+
+  ind_cat <- grep(paste0("Catch_fut_", scen), names(tmp))
+  ind_ssb <- grep(paste0("SSB_fut_", scen), names(tmp))
+
+  cat_fut <- tmp[[ind_cat]] %>%
+    as_tibble() %>%
+    rename(year=V1,catch=V2)
+
+  ssb_fut <- tmp[[ind_ssb]] %>%
+    as_tibble() %>%
+    rename(year=V1, ssb=V2, sd=V3, lb=V4, ub=V5)
+
+  if(scen == 2) scn <- 1.5
+  if(scen == 3) scn <- 2
+
+  quant_fut <- full_join(cat_fut,ssb_fut) %>%
+    mutate(file=nm, scenario = paste0("F",yr_curr," x",scn)) %>%
+     separate(file, into=c("hyp","file"), sep="_For_R_") %>%
+     mutate(stock = str_remove(file, "_tac.rep")) %>%
+     select(-file)
+
+  return(quant_fut)
+}
+
+filenamesF <- repfilenames[grep("_tac.rep", repfilenames)]
+
 quants_fut <- map_dfr(repfilenames, fn_pullfuts) %>%
-			separate(file, into=c("hyp","file"), sep="_For_R_") %>%
-			separate(file, into=c("stock","scenario"), sep="_") %>%
-			mutate(scenario=str_remove(scenario,".rep")) %>%
-			mutate(scenario=case_when(scenario=="tac15" ~ paste0("TAC",yr_curr, " 1.15x"),
-										scenario=="tac20" ~ paste0("TAC",yr_curr, " 1.2x"),
-										scenario=="tac" ~ paste0("TAC",yr_curr)))
+  separate(file, into=c("hyp","file"), sep="_For_R_") %>%
+  separate(file, into=c("stock","scenario"), sep="_") %>%
+  mutate(
+    scenario = str_remove(scenario,".rep"),
+    scenario = str_replace(scenario, "tac", paste0("TAC",yr_curr, " x1."))
+) %>%
+  bind_rows(map_dfr(filenamesF, fn_pullfutF, scen = 2)) %>%
+  bind_rows(map_dfr(filenamesF, fn_pullfutF, scen = 3))
 
 quants_proj <- quants_fut %>%
 				select(year, catch, ssb, sd, hyp, stock, scenario) %>%
@@ -105,13 +150,14 @@ quants_curr <- quants_proj %>%
 				arrange(year,hyp,stock) %>%
 				group_by(year,hyp,stock) %>%
 				fill(everything(),.direction="updown") %>%
-				drop_na() %>%
+				drop_na(catch, ssb) %>%
 				ungroup()
 
 quants_proj <- quants_proj %>%
 				anti_join(quants_rm) %>%
 				bind_rows(quants_curr) %>%
-				distinct()
+				distinct() %>%
+
 
 yrs2use <- c(yr_curr+2, yr_curr+6, yr_curr+10)
 
@@ -131,6 +177,7 @@ risk_table <- quants_fut %>%
 				select(-bmsy, -sd) %>%
 				pivot_wider(names_from=year,values_from=c(ssb,prob), names_sep="_",names_vary="slowest") %>%
 				left_join(risk_table_cat) %>%
+				arrange(hyp) %>%
 				write_csv("risk_tables/tac.csv")
 
 ggplot(quants_proj %>% filter(year>=2000)) +
