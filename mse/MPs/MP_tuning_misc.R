9f73f6a3f29d18fdca844989cb50b6d44f748b5e
tcarruth <tomsinbox@gmail.com>
2024-12-03 09:46:00 -0800

    Repo too big
    Will reduce and push later, sry!

--- mse/MPs/MP_tuning_misc.R
+++
@@ -1,8 +0,0 @@
-# MP tuning
-
-Btuning = function(MSEobj,Btarg=1,xs = seq(0.3,0.9,by=0.1),yr = 50,plot=T){
-  Best = apply(MSEobj@SB_SBMSY[,,yr],2,mean)
-  xtarg = approx(Best,xs,Btarg)$y
-  if(plot){plot(xs,Best);abline(h=Btarg,v=xtarg,col="red")}
-  xtarg
-}
